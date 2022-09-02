cb028122926940847e9ba9d71324691cb1d8e7b1
rogers <variablecode404@gmail.com>
2022-09-02 03:40:47 +0300

    second

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var nav2 = document.getElementById('nav2');
 
 
 nav.innerHTML = `
-    <div class="px-20 px-10-lg normal px-0-md grid-container py-3 justify-between">
+    <div class="grid-container px-2 py-1 align-items-center justify-between">
         <div class="">
-            <a href="index.html"><p class="text-lg pl-2-md py-1-md normal">Variable code</p></a>
+            <a href="index.html"><p class="title py-3">Variable code</p></a>
         </div>
         <div class="grid-container w-100-md items-center justify-center">
             <a class="w-100-md py-2-md" href="download.html"><p class="px-2">Download</p></a>
@@ -16,7 +16,9 @@ nav.innerHTML = `
             <a class="w-100-md py-2-md"><p class="px-2">Library</p></a>
             <a class="w-100-md py-2-md"><p class="px-2">News</p></a>
         </div>
-        <div class="w-100-md py-1-md"><p class="pl-2-md"><i class="fas fa-moon"></i></p></div>
+        <div class="w-100-md py-1-md">
+            <a href="download.html"><p class="py-2 px-4 blue text-white">install vc now</p></a>
+        </div>
     </div>
 `;
 
--- main.js
+++ main.js
@@ -18,37 +18,36 @@ toogle.addEventListener('click', ()=>{
 });
 
 
-overviewToogle.addEventListener('click', ()=>{
+function toggleOverview(){
+    console.log('whatvyfyvy')
     if (overview.classList.contains('d-none-md')){
         overview.classList.remove("d-none-md");
-        overviewToogle.classList.add('text-red-500');
-        overviewToogle.classList.add('bold');
-        overviewToogle.classList.remove('b-gray-500');
     } else {
         overview.classList.add("d-none-md");
-        overviewToogle.classList.remove('text-red-500');
-        overviewToogle.classList.remove('bold');
-        overviewToogle.classList.add('b-gray-500');
-
     }
+}
 
-    console.log('clicked');
-});
+
+overviewToogle.onclick = toggleOverview;
+
 
 
 function navBar(){
     nv.innerHTML = `
-        <div class="px-20 px-10-lg normal px-0-md grid-container py-3 justify-between">
+        <div class="z-4 normal grid-container align-items-center py-1 justify-between">
             <div class="">
-                <a href="index.html"><p class="text-lg pl-2-md py-1-md normal">Variable code</p></a>
+                <a href="index.html"><p class="title px-2 py-3">Variable code</p></a>
             </div>
             <div class="grid-container w-100-md items-center justify-center">
                 <a class="w-100-md py-2-md" href="download.html"><p class="px-2">Download</p></a>
+                <a class="w-100-md py-2-md" href="started.html"><p class="px-2">Docs</p></a>
                 <a class="w-100-md py-2-md"><p class="px-2">Contribute</p></a>
                 <a class="w-100-md py-2-md"><p class="px-2">Library</p></a>
                 <a class="w-100-md py-2-md"><p class="px-2">News</p></a>
             </div>
-            <div class="w-100-md py-1-md"><p class="pl-2-md"><i class="fas fa-moon"></i></p></div>
+            <div class="w-100-md">
+                <a href="download.html"><p class="py-2 px-4 blue text-white">install vc now</p></a>
+            </div>
         </div>
     `;
 }
@@ -56,10 +55,10 @@ function navBar(){
 
 function render(){
     sd.innerHTML = `
-        <div class="pl-7 pt-2 sd">
-            <a href="started.html"><p>Get started.</p></a>
-            <a href="basics.html"><p class="">Basics</p></a>
-            <div class="pl-2 inner">
+        <div class="pl-7 pos-relative z-4 pt-2">
+            <a href="started.html"><p class="uppercase ">Get started.</p></a>
+            <a href="basics.html"><p class="uppercase pt-1">Basics</p></a>
+            <div class="inner">
                 <a href="basics.html#comments"><p class="">1. Comments</p></a>
                 <a href="basics.html#datatypes"><p class="">2. Datatypes</p></a>
                 <a href="basics.html#variables"><p class="">3. Variables</p></a>
@@ -68,29 +67,30 @@ function render(){
                 <a href="basics.html#while"><p class="">6. Whileloop</p></a>
                 <a href="basics.html#for"><p class="">7. Forloop</p></a>
             </div>
-            <a href="modules.html"><p class="">Modules</p></a>
-            <div class="pl-2 inner">
-                <a href="Path.html"><p class="">1. Path</p></a>
-                <a href="File.html"><p class="">2. File</p></a>
-                <a href="Time.html"><p class="">3. Time</p></a>
-                <a href="Environ.html"><p class="">4. Environ</p></a>
-                <a href="Utils.html"><p class="">5. Utils</p></a>
-                <a href="Socket.html"><p class="">6. Socket</p></a>
-                <a href="Net.html"><p class="">7. Net</p></a>
-                <a href="Thread.html"><p class="">8. Thread</p></a>
-                <a href="Random.html"><p class="">9. Random</p></a>
-                <a href="Math.html"><p class="">10. Math</p></a>
+            <a href="modules.html"><p class="mt-3 uppercase">Modules</p></a>
+            <div class="inner">
+                <a href="Path.html"><p class="">Path</p></a>
+                <a href="File.html"><p class="">File</p></a>
+                <a href="Time.html"><p class="">Time</p></a>
+                <a href="Environ.html"><p class="">Environ</p></a>
+                <a href="Utils.html"><p class="">Utils</p></a>
+                <a href="Socket.html"><p class="">Socket</p></a>
+                <a href="Net.html"><p class="">Net</p></a>
+                <a href="Thread.html"><p class="">Thread</p></a>
+                <a href="Random.html"><p class="">Random</p></a>
+                <a href="Math.html"><p class="">Math</p></a>
             </div>
         </div>
     `;
 }
 
+
 function Overview(){
-    overview.innerHTML = `
-        <div class="pl-2 py-4 bg-white sd sd2">
+    overview.innerHTML += `
+        <div class="pl-2 overV pb-3">
             <a href="started.html"><p class="page">Get started.</p></a>
             <a href="basics.html"><p class="page">Basics</p></a>
-            <div class="pl-2 inner2">
+            <div class="">
                 <a href="basics.html#comments"><p class="">Comments</p></a>
                 <a href="basics.html#datatypes"><p class="">Datatypes</p></a>
                 <a href="basics.html#variables"><p class="">Variables</p></a>
@@ -100,7 +100,7 @@ function Overview(){
                 <a href="basics.html#for"><p class="">Forloop</p></a>
             </div>
             <a href="modules.html"><p class="page">Modules</p></a>
-            <div class="pl-2 inner2">
+            <div class="">
                 <a href="Path.html"><p class="">Path</p></a>
                 <a href="File.html"><p class="">File</p></a>
                 <a href="Time.html"><p class="">Time</p></a>
@@ -116,17 +116,9 @@ function Overview(){
     `
 }
 
-function footer(){
-    foot.innerHTML = `
-            <div class="py-3 grid-container justify-around">
-                <a><p class=""></p></a>
-                <p class="">Variable Code</p>
-            </div>
-    `;
-}
 
 navBar();
 render();
 
-footer();
+// footer();
 Overview()
